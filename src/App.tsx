2b82c3dcfb8d94707ff5534d747ca7843193a716
Yousaf Azabi <joeazabi@gmail.com>
2021-06-30 21:37:32 +0100

    set apollo provider and client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import './App.css';
+import Home from './component/Home'
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: "Bearer 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"
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   );
 }
 
