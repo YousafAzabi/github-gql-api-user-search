b045e973ce56227ad6eb8e104570f5286c017a32
Yousaf Azabi <joeazabi@gmail.com>
2021-06-30 22:59:35 +0100

    set token in .env file

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import './App.css';
 import Home from './component/Home'
 
+const token = process.env.REACT_APP_API_TOKEN;
+
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    authorization: "Bearer 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"
+    authorization: `Bearer ${token}`
   }
 });
 
