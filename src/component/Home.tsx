e3fb5e27abe20f9de15f6efee6a1ab360fdc1ad8
Yousaf Azabi <joeazabi@gmail.com>
2021-07-01 00:48:59 +0100

    set apollo gql query

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import List from './List';
 import Search from './Search';
+import { useQuery, gql } from '@apollo/client';
+
 
 export default function Home() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      search(query: "yousaf", type: USER, first: 5) {
+        nodes {
+          ...on User {
+            name
+            avatarUrl
+          }
+        }
+        userCount
+      }
+    }
+  `);
+
+  if (error) { return <p>error</p> }
+  if (loading) { return <p>Loading</p> }
+
   return (
     <div className="home">
       <Search />
